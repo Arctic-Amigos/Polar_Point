50c8ca4f5dbc21a0a1b65045c6cb53ae6c95e359
malatestafrank <frankmalatesta@ufl.edu>
2024-02-13 16:35:17 -0500

    Updated Inventory Script
    Added GetInventory, SetInventory, IndexIsEmpty, and FirstEmpty

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,8 +7,8 @@ public class Inventory : MonoBehaviour
     // First 3 spots in inventory will be static for hand, pickaxe, brush. Last 5 spots will be open for fossils/bones
     // Hand, Pickaxe, Brush | Fossil, Fossil, Fossil, Fossil, Fossil
     // -3  , -2     , -1    | 0     , 1     , 2     , 3     , 4
-    string[] inventory = new string[5]; // Inventory spots that will be filled up with fossils/bones. Type can be changed to fossil/bone type as needed
-    int inventory_pos; // Position in inventory that will be scrolled through
+    string[] fossil_inventory = new string[5]; // Inventory spots that will be filled up with fossils/bones. Type can be changed to fossil/bone type as needed
+    public int inventory_pos; // Position in inventory that will be scrolled through
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +18,7 @@ public class Inventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Will add scrolling and number button functionality
+        // Number button functionality
         if (Input.GetKeyDown(KeyCode.Alpha1))
             inventory_pos = -3;
         if (Input.GetKeyDown(KeyCode.Alpha2))
@@ -35,5 +35,36 @@ public class Inventory : MonoBehaviour
             inventory_pos = 3;
         if (Input.GetKeyDown(KeyCode.Alpha8))
             inventory_pos = 4;
+        // Scrolling functionality
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+    }
+    public string GetInventory(int index)
+    {
+        return fossil_inventory[index];
+    }
+
+    public void SetInventory(int index, string item)
+    {
+        fossil_inventory[index] = item;
+    }
+    public bool IndexIsEmpty(int index) // Returns whether an index is empty
+    {
+        if (fossil_inventory[index] == null) 
+            return true;
+        return false;
+    }
+    public int FirstEmpty() // Returns the index of the first empty index or 9 if all inventory slots are full
+    {
+        if (IndexIsEmpty(0))
+            return 0;
+        else if (IndexIsEmpty(1))
+            return 1;
+        else if (IndexIsEmpty(2))
+            return 2;
+        else if (IndexIsEmpty(3))
+            return 3;
+        else if (IndexIsEmpty(4))
+            return 4;
+        return 9;
     }
 }
