282a6175bea3bbbafa63ec062a8d4a856b9c109a
malatestafrank <frankmalatesta@ufl.edu>
2024-03-26 16:18:48 -0400

    IsWorkbenchInteracting Function
    Function added for Blake

--- Assets/Scenes/Scripts/Workbench Cursor Follow.cs
+++ Assets/Scenes/Scripts/Workbench Cursor Follow.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class WorkbenchCursorFollow : MonoBehaviour
 {
+    Inventory inventory;
+    public int desired_pos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventory = GetComponent<Inventory>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(inventory.inventory_pos == 2)
+        {
+            Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            cursorPosition.z = 0f;
+            transform.position = cursorPosition;
+        }
     }
 }
