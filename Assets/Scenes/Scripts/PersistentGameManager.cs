2e63d7b484b535e0489c78e8bcad2f7e304c4762
malatestafrank <frankmalatesta@ufl.edu>
2024-04-19 12:05:01 -0400

    Persistent + Base Fixes
    Player inventory is now persistent. Inventory items are now properly displayed in the home base

--- Assets/Scenes/Scripts/PersistentGameManager.cs
+++ Assets/Scenes/Scripts/PersistentGameManager.cs
@@ -10,8 +10,6 @@ public class PersistentGameManager : MonoBehaviour
     public static string inventory2 = null;
     public static string inventory3 = null;
     public static string inventory4 = null;
-    GameObject player;
-    Inventory inventory;
 
 
     // Start is called before the first frame update
@@ -28,12 +26,12 @@ public class PersistentGameManager : MonoBehaviour
 
     public void SaveInfo()
     {
-        var i = new Inventory();
-        inventoryPos = i.inventory_pos;
-        inventory0 = i.fossil_inventory[0];
-        inventory1 = i.fossil_inventory[1];
-        inventory2 = i.fossil_inventory[2];
-        inventory3 = i.fossil_inventory[3];
-        inventory4 = i.fossil_inventory[4];
+        Inventory inv = GameObject.FindObjectOfType<Inventory>();
+        inventoryPos = inv.inventory_pos;
+        inventory0 = inv.fossil_inventory[0];
+        inventory1 = inv.fossil_inventory[1];
+        inventory2 = inv.fossil_inventory[2];
+        inventory3 = inv.fossil_inventory[3];
+        inventory4 = inv.fossil_inventory[4];
     }
 }
